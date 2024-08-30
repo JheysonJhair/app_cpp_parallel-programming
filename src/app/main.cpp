5538331c5dc0734075362515733ec4a17daf29c2
JheysonJhair <jhair2003unamba@gmail.com>
2024-08-29 19:28:48 -0500

    feat: se agrego el tiempo de demora

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,27 +3,30 @@
 #include <thread>
 #include <memory>
 #include <windows.h>  
+#include <chrono>
 
 #include "../class/Funciones.h"
 #include "../class/Cliente.h"
 
 const int NUM_THREADS = 4; 
+
 //------------------------------------------------------------------------------------- BUSCAR CON HILOS Y ASIGNACION DE NUCLEOS
 void buscarConHilos(const std::string& archivo, const std::string& ruc) {
+    auto start = std::chrono::high_resolution_clock::now(); 
+
     std::vector<Cliente> clientes;
     cargarDatos(archivo, clientes);
-    
+
     int numClientes = clientes.size();
     int chunkSize = numClientes / NUM_THREADS;
     std::vector<std::thread> threads;
     std::shared_ptr<Cliente> resultado = nullptr;
-    
+
     for (int i = 0; i < NUM_THREADS; ++i) {
         int start = i * chunkSize;
         int end = (i == NUM_THREADS - 1) ? numClientes : (i + 1) * chunkSize;
-        
-        threads.emplace_back([&, i, start, end]() {
 
+        threads.emplace_back([&, i, start, end]() {
             HANDLE thread = GetCurrentThread();
             DWORD_PTR mask = 1 << (i % std::thread::hardware_concurrency());
             SetThreadAffinityMask(thread, mask);
@@ -31,52 +34,69 @@ void buscarConHilos(const std::string& archivo, const std::string& ruc) {
             buscarClienteEnRango(clientes, ruc, start, end, resultado);
         });
     }
-    
+
     for (auto& thread : threads) {
         thread.join();
     }
-    
+
     if (resultado) {
         std::cout << "Cliente encontrado: " << resultado->nombre << std::endl;
     } else {
         std::cout << "Cliente no encontrado." << std::endl;
     }
+
+    auto end = std::chrono::high_resolution_clock::now(); 
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Tiempo: " << elapsed.count() << " segundos" << std::endl;
 }
 
 //------------------------------------------------------------------------------------- BUSCAR CON VECTOR Y PROGRAMACION FUNCIONAL
 void buscarConVector(const std::string& archivo, const std::string& ruc) {
+    auto start = std::chrono::high_resolution_clock::now(); 
+
     std::vector<Cliente> clientes;
     cargarDatos(archivo, clientes);
-    
+
     std::shared_ptr<Cliente> resultado = buscarClienteEnVector(clientes, ruc);
-    
+
     if (resultado) {
         std::cout << "Cliente encontrado: " << resultado->nombre << std::endl;
     } else {
         std::cout << "Cliente no encontrado." << std::endl;
     }
+
+    auto end = std::chrono::high_resolution_clock::now(); 
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Tiempo: " << elapsed.count() << " segundos" << std::endl;
 }
 
 //------------------------------------------------------------------------------------- BUSCAR SIN PARALELISMO
 void buscarSinParalelismo(const std::string& archivo, const std::string& ruc) {
+    auto start = std::chrono::high_resolution_clock::now(); 
+
     std::shared_ptr<Cliente> resultado = buscarClienteEnArchivo(archivo, ruc);
-    
+
     if (resultado) {
         std::cout << "Cliente encontrado: " << resultado->nombre << std::endl;
     } else {
         std::cout << "Cliente no encontrado." << std::endl;
     }
+
+    auto end = std::chrono::high_resolution_clock::now(); 
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Tiempo: " << elapsed.count() << " segundos" << std::endl;
 }
+
 
 //------------------- MAIN
 int main() {
-    std::string archivo = "../data/padron_reducido_ruc.txt";
+    std::string archivo = "../data/padron_ruc.txt";
     
     while (true) {
         std::cout << "==============================================================================================" << std::endl;
         std::cout << "|                                        MENU PRINCIPAL                                      |" << std::endl;
         std::cout << "==============================================================================================" << std::endl;
-        std::cout << "|1. Buscar datos de un cliente usando hilos y asignación de tareas a núcleos del procesador. |" << std::endl;
+        std::cout << "|1. Buscar datos de un cliente usando hilos y asignación de tareas a nucleos del procesador. |" << std::endl;
         std::cout << "|2. Buscar datos de un cliente usando programacion funcional en un vector.                   |" << std::endl;
         std::cout << "|3. Buscar datos de un cliente sin paralelismo (optimizacion).                               |" << std::endl;
         std::cout << "|--------------------------------------------------------------------------------------------|" << std::endl;
@@ -105,7 +125,7 @@ int main() {
                 buscarSinParalelismo(archivo, rucABuscar);
                 break;
             default:
-                std::cout << "Opción no válida. Inténtelo de nuevo." << std::endl;
+                std::cout << "Opcion no valida. Intentelo de nuevo." << std::endl;
                 break;
         }
     }
